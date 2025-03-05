566e568081e613049e43147215f76eb951e9544e
jhuniel-galang <jhunielstorage1@gmail.com>
2025-03-05 15:27:03 +0800

    2nd commit

--- lib/main.dart
+++ lib/main.dart
@@ -202,7 +202,7 @@ class _WifiPageState extends State<WifiPage> {
             ),
             Divider(),
 
-            // Saved Networksssss
+            // Saved Networksssssssss
             if (isWifiEnabled) ...[
               Padding(
                 padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
